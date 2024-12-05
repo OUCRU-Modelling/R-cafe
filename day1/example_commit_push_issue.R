90a0e9bd31b95c2ac8ab5f9510e13078a824bd32
Hung Tran Thai <hungtt@oucru.org>
2024-12-05 11:53:47 +0700

    fix typo error
    Fix typo error in example_commit_issur.R from value T to 7

--- day1/example_commit_push_issue.R
+++ day1/example_commit_push_issue.R
@@ -1,5 +1,5 @@
 # Create a sequence of numbers
-numbers <- c(3, 3, 5, T, 7, 8)
+numbers <- c(3, 3, 5, 7, 7, 8) #fix typo error value T to correct value 7 - hungtt
 
 # Calculate the sum of numbers (with a typo in the function name)
 mean <- mean(numbers)
