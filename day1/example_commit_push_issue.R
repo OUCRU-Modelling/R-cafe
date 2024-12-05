215c646ad78b2038d8b95f5b8416c1ba22ec0e40
Hung Tran Thai <hungtt@oucru.org>
2024-12-05 11:05:28 +0700

    add Rproject, renv, script files
    -Add Rproject, renv
    -Add example_commit_push_issue.R to day1 folder

---
+++ day1/example_commit_push_issue.R
@@ -0,0 +1,8 @@
+# Create a sequence of numbers
+numbers <- c(3, 3, 5, T, 7, 8)
+
+# Calculate the sum of numbers (with a typo in the function name)
+mean <- mean(numbers)
+
+# Print the result
+print(mean)
